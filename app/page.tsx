3fef253c46134f85ae1b7213418dc0fbe692e63a
luka_C.B <lukaaslamazashvili20@gmail.com>
2025-02-17 18:46:20 +0400

    Adding Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.scss";
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <h1>This is the main page</h1>
+    </main>
+  );
+}
